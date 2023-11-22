1ad3ab46061bade58eee5aa1cebd21bdd24634e0
alegreco7 <alexgreco2002@gmail.com>
2023-11-22 17:12:37 +0100

    multipli 3 e 5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
  // Loop da 1 a 100
  for (let i = 1; i <= 100; i++) {
-    
+
+    // Verifica se il numero è multiplo di 3 e 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } 
+      
  }
