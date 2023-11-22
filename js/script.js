20c91924f2c28cb8f867a7d2e693144d75bf9cc7
alegreco7 <alexgreco2002@gmail.com>
2023-11-22 17:18:23 +0100

    ciclo completo

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
       } 
-      
- }
+
+       // Verifica se il numero è multiplo di 3
+       else if (i % 3 === 0) {
+        console.log("Fizz");
+      } 
+
+      // Verifica se il numero è multiplo di 5
+      else if (i % 5 === 0) {
+        console.log("Buzz");
+      } 
+
+       // Se il numero non è multiplo né di 3 né di 5, stampa il numero stesso
+       else {
+        console.log(i);
+      }
+}
